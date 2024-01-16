eabf82647601d1f37edb17a115e8a1b6a445ef1b
Nicolas Garza <nicolas.garza@gmail.com>
2024-01-16 15:35:22 -0600

    tokenizer and repl

--- token/token.go
+++ token/token.go
@@ -14,6 +14,16 @@ const (
 	// Operators
 	ASSIGN = "="
 	PLUS = "+"
+	MINUS = "-"
+	BANG = "!"
+	ASTERISK = "*"
+	SLASH = "/"
+
+	LT = "<"
+	GT = ">"
+
+	EQ = "=="
+	NOT_EQ = "!="
 
 	// Delimiters
 	COMMA = ","
@@ -27,6 +37,13 @@ const (
 	// Keywords
 	FUNCTION = "FUNCTION"
 	LET = "LET"
+	TRUE = "TRUE"
+	FALSE = "FALSE"
+	IF = "IF"
+	ELSE = "ELSE"
+	RETURN = "RETURN"
+
+
 )
 
 type Token struct {
@@ -37,6 +54,11 @@ type Token struct {
 var keywords = map[string]TokenType{
 	"fn": FUNCTION,
 	"let": LET,
+	"true": TRUE,
+	"false": FALSE,
+	"if": IF,
+	"else": ELSE,
+	"return": RETURN,
 }
 
 func LookupIdent(ident string) TokenType {
