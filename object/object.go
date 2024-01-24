1dfa29d7b34a21097a4d7fe8e4fccb3e77db0aa2
Nicolas Garza <nicolas.garza@gmail.com>
2024-01-24 11:01:04 -0600

    more eval stuff

--- object/object.go
+++ object/object.go
@@ -1,36 +1,35 @@
-import "fmt"
-
 package object
+
+import "fmt"
 
 type ObjectType string
 
 type Object interface {
-	Tpye() ObjectType
+	Type() ObjectType
 	Inspect() string
 }
-
-type ObjectType string
 
 const (
 	INTEGER_OBJ = "INTEGER"
 	BOOLEAN_OBJ = "BOOLEAN"
-	NULL_OBJ = "NULL"
+	NULL_OBJ    = "NULL"
 )
 
 type Integer struct {
 	Value int64
 }
 
-func (i *Integer) Inspect() string { return fmt.Sprintf("%d", i.Value) }
+func (i *Integer) Inspect() string  { return fmt.Sprintf("%d", i.Value) }
 func (i *Integer) Type() ObjectType { return INTEGER_OBJ }
 
 type Boolean struct {
-	Value book
+	Value bool
 }
 
 func (b *Boolean) Type() ObjectType { return BOOLEAN_OBJ }
-func (b *Boolean) Inspect() string { return fmt.Sprintf("%b", b.Value)}
+func (b *Boolean) Inspect() string  { return fmt.Sprintf("%t", b.Value) }
 
 type Null struct{}
+
 func (n *Null) Type() ObjectType { return NULL_OBJ }
-func (n *Null) Inspect() string { return "null" }
+func (n *Null) Inspect() string  { return "null" }
