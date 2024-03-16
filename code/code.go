9027d9726881c1b64e82b59e93504c6da0c21301
nicolasgarza <nicolas.garza@gmail.com>
2024-03-15 23:16:57 -0500

    array literals

--- code/code.go
+++ code/code.go
@@ -32,6 +32,8 @@ const (
 
 	OpGetGlobal
 	OpSetGlobal
+
+	OpArray
 )
 
 type Instructions []byte
@@ -101,6 +103,7 @@ var definitions = map[Opcode]*Definition{
 	OpNull:          {"OpNull", []int{}},
 	OpGetGlobal:     {"OpGetGlobal", []int{2}},
 	OpSetGlobal:     {"OpSetGlobal", []int{2}},
+	OpArray:         {"OpArray", []int{2}},
 }
 
 func Lookup(op byte) (*Definition, error) {
